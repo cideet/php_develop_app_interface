f9131a6d9353abe9a1960d0bae1c8572393141d8
张三丰 <123456@qq.com>
2017-03-31 19:26:32 +0800

    PHP输出JSON（http://127.0.0.27/response.php）

---
+++ response.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/3/31
+ * Time: 19:17
+ */
+
+$arr = array(
+    'id' => 1,
+    'name' => 'zhangsanfen',
+    'description' => 'PHP输出JSON'
+);
+echo json_encode($arr);
