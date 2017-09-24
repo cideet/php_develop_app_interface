500b22b534b6f1ef208475fb5fed283edefdb533
zhangsanfeng <123456@qq.com>
2017-09-24 14:08:22 +0800

    输出xml

---
+++ code/04.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/9/24
+ * Time: 13:57
+ */
+
+function xml()
+{
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<root>';
+    $xml .= '<code>200</code>';
+    $xml .= '<message>数据返回成功</message>';
+    $xml .= '<data>';
+    $xml .= '<id>1</id>';
+    $xml .= '<name>zhangsanfen</name>';
+    $xml .= '</data>';
+    $xml .= '</root>';
+    echo $xml;
+}
+
+xml();
