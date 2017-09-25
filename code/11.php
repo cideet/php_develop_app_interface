64df61aa740653747c45aaddd386708f757bc779
张三丰 <zhangyz@gveoe.cn>
2017-09-25 17:15:54 +0800

    封装“按JOSN或XML方式输出通信数据”

---
+++ code/11.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/25 0025
+ * Time: 17:00
+ */
+
+require('./09.php');
+
+$arr = array(
+    'id' => 1,
+    'name' => '张三丰'
+);
+
+echo(Response::show1(200, 'success', $arr, 'json'));
